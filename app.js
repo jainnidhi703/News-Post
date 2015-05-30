f9eead5b5d8dcd489dfee28afaddba777ec394cc
jainnidhi703 <jainnidhi703@gmail.com>
2015-05-30 22:59:30 +0530

    with comments

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-var app = angular.module('NewsPost', []);
+var app = angular.module('NewsPost', ['ui.router']);
 
 app.factory('posts', [function(){
   var o = {
     posts: []
   };
   return o;
+}]);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl'
+        })
+
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl'
+        })
+
+    $urlRouterProvider.otherwise('home');
 }]);
 
 app.controller('MainCtrl', [
@@ -19,7 +41,11 @@ function($scope, posts){
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
+        upvotes: 0 , 
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
       });
       $scope.title = '';
       $scope.link = '';
@@ -30,3 +56,21 @@ function($scope, posts){
     };
     
 }]);
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0  
+      });
+      $scope.body = '';
+    };
+}]);
+
